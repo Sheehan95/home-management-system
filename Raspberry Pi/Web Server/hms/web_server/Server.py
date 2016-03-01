a439d203d5cd304367973ca7864dddf161f0d82f
Alan Sheehan <al.j.sheehan@gmail.com>
2016-03-01 19:35:32 +0000

    Added detailed commenting to Pi server, including basic Python details

--- Raspberry Pi/Web Server/hms/web_server/Server.py
+++ Raspberry Pi/Web Server/hms/web_server/Server.py
@@ -5,10 +5,10 @@ from hms.web_server.Sensors import TemperatureSensor, MotionSensor
 HOST_NAME = '127.0.0.1'
 PORT_NUMBER = 8080
 
-
+# class that extends the BaseHTTPRequestHandler class
 class WebServer(BaseHTTPServer.BaseHTTPRequestHandler):
 
-    # URLS being handled
+    # String constants for the URLs being served
     INDEX = '/'
     TEMPERATURE = '/temperature'
     MOTION = '/motion'
@@ -17,19 +17,23 @@ class WebServer(BaseHTTPServer.BaseHTTPRequestHandler):
     temp_sensor = TemperatureSensor()
     motion_sensor = MotionSensor()
 
+    # overwritten function
     def do_HEAD(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+    # overwritten function that handles HTTP GET requests
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
 
+        # the path is the URL being viewed. if path == '/temperature', the URL is localhost:8080/temperature
         if self.path == WebServer.INDEX:
             print "Index"
 
         elif self.path == WebServer.TEMPERATURE:
+            # adds the key-value pair to the header
             self.send_header('temperature', WebServer.temp_sensor.get_temp())
 
         elif self.path == WebServer.MOTION:
@@ -43,7 +47,7 @@ class WebServer(BaseHTTPServer.BaseHTTPRequestHandler):
 
         self.end_headers()
 
-
+# equivalent to the main function
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), WebServer)
